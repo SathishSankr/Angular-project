66f1d43c0729e3d4be102ed4add662ba322ccfec
Sathish <sathishtmsa@gmail.com>
2020-09-13 22:16:07 +0530

    added languages

--- src/app/others/language.pipe.ts
+++ src/app/others/language.pipe.ts
@@ -7,15 +7,37 @@ export class LanguagePipe implements PipeTransform {
   languageSet = {
     ENGLISH: {
       HELLO: 'Hello',
-      LOGIN: 'Login'
+      LOGIN: 'Login',
+      SIGNUP: 'SignUp',
+      'USER NAME': 'UserName',
+      PASSWORD: 'Password',
+      'FORGET PASSWORD': 'Forget Password',
+      CANCEL: 'Cancel',
+      'RE-TYPE PASSWORD': 'Re-Type Password',
+      'SIGN IN': 'Sign In'
     },
     TAMIL: {
       HELLO: 'வணக்கம்',
-      LOGIN: 'உள்நுழைய'
+      LOGIN: 'உள்நுழைய',
+      SIGNUP: 'பதிவுபெறுதல்',
+      'USER NAME': 'பயனர்பெயர்',
+      PASSWORD: 'கடவுச்சொல்',
+      'FORGET PASSWORD': 'கடவுச்சொல்லை மறந்துவிட்டீர்களா',
+      CANCEL: 'ரத்துசெய்',
+      'RE-TYPE PASSWORD': 'கடவுச்சொல்லை மீண்டும் தட்டச்சு செய்க',
+      'SIGN IN': 'உள்நுழைக'
+
     },
     HINDI: {
       HELLO: 'नमस्ते',
-      LOGIN: 'लॉग इन करें'
+      LOGIN: 'लॉग इन करें',
+      SIGNUP: 'साइन अप करें',
+      'USER NAME': 'उपयोगकर्ता नाम',
+      PASSWORD: 'कुंजिका',
+      'FORGET PASSWORD': 'पासवर्ड भूल गए?',
+      CANCEL: 'रद्द करना',
+      'RE-TYPE PASSWORD': 'पासवर्ड फिर से लिखें',
+      'SIGN IN': 'साइन इन करें'
     }
   }
   transform(value: string, ...args: unknown[]): string {
